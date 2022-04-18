cb518c04a115fc8c91b132686e9f2b950d3d9a48
avanidhalgara <avanidhalgara01@gmail.com>
2022-04-18 00:08:51 -0500

    initial commit

---
+++ My-first-java-project-1/src/main/java/JavaMain/Main.java
@@ -0,0 +1,30 @@
+package JavaMain;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+//		1st question
+		
+		int num1 = 10;
+		int num2 = 5;
+		int sum = num1+num2;
+		System.out.println("Two numbers of sum is: "+sum);
+		
+		
+//		2nd question
+		double number1 = 10.98;
+		double number2 = 20.97;
+		double sum1= number1 + number2;
+		System.out.println("Two number of sum is: "+sum1);
+		
+//		3rd question
+		double number_1 = 20.98;
+		int number_2 = 10;
+		int sum_1 = (int)number_1 + number_2;
+		System.out.println("The sum of two number is: "+sum_1);
+	}
+
+}
